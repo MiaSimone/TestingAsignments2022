bbdc8366fcd24dbcf4c37dd1cd54a474f26bc27f
MiaSimone <cph-md291@cphbusiness.dk>
2022-09-20 14:05:57 +0200

    created assignment1 project

---
+++ Assignment1/TestAssignment1/src/main/java/test/assignments22/testassignment1/TestAssignment1Application.java
@@ -0,0 +1,13 @@
+package test.assignments22.testassignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAssignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAssignment1Application.class, args);
+	}
+
+}
---
+++ Assignment1/TestAssignment1/src/test/java/test/assignments22/testassignment1/TestAssignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package test.assignments22.testassignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAssignment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
